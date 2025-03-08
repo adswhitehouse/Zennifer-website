bc68bafb77c0d445c2c62b2ab85616c77335241f
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-08 14:53:02 +0000

    made responsove on different screen sizes

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 let menu = document.querySelector(".jsMenu")
 let ul = document.querySelector("ul")
 
+// Toggle nav menu visibility on small screens
 menu.addEventListener("click", () => {
   ul.classList.toggle("show-nav")
 })
 
+// Remove nav menu when screen is resized outside of media query 
 window.addEventListener("resize", () => {
   if (window.innerWidth > 960) {
     ul.classList.remove("show-nav")
