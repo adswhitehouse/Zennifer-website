af48ce948f24642657be9db13ca91948f4685ebb
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-08 15:35:28 +0000

    added comments

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// Variables
-let menu = document.querySelector(".jsMenu")
-let ul = document.querySelector("ul")
+document.addEventListener("DOMContentLoaded", () => {
+  // Variables
+  let menu = document.querySelector(".jsMenu");
+  let ul = document.querySelector("ul");
 
-// Toggle nav menu visibility on small screens
-menu.addEventListener("click", () => {
-  ul.classList.toggle("show-nav")
-})
+  // Toggle nav menu visibility on small screens
+  menu.addEventListener("click", () => {
+    ul.classList.toggle("show-nav");
+  });
 
-// Remove nav menu when screen is resized outside of media query 
-window.addEventListener("resize", () => {
-  if (window.innerWidth > 960) {
-    ul.classList.remove("show-nav")
-  }
-})
+  // Remove nav menu when screen is resized outside of media query
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 960 && ul.classList.contains("show-nav")) {
+      ul.classList.remove("show-nav");
+    }
+  });
+});
